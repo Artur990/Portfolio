dcbc3274327a14e89fd19063539271a74cb45825
Artur <102363429+Artur990@users.noreply.github.com>
2023-04-01 13:30:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/Artur-Grzębski", // podaj nazwę swojego projektu
-  assetPrefix: "/Portfoliou", // podaj nazwę swojego projektu
+
 };
 
 module.exports = nextConfig;
